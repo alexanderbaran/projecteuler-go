ddd1c913bf55a8c9072fa81604c4fe75b7d157d5
Alexander Baran <alexanderbaran@live.com>
2016-11-09 14:57:42 +0100

    asdf

---
+++ main/018.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+type edge struct {
+	to     int
+	weight int
+}
+
+func main() {
+	ints := []int{}
+	f, err := os.Open("018.txt")
+	if err != nil {
+		fmt.Println(err)
+	}
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		for _, v := range strings.Split(scanner.Text(), " ") {
+			d, _ := strconv.Atoi(v)
+			ints = append(ints, d)
+		}
+	}
+	if err := scanner.Err(); err != nil {
+		fmt.Println(err)
+	}
+
+	adj := make([][]edge, len(ints))
+	_ = adj
+}
