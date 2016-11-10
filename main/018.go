ff75f0c8714bd8378365ff0e62dff19c8c181273
Alexander Baran <alexanderbaran@live.com>
2016-11-10 17:04:42 +0100

    asdf

--- main/018.go
+++ main/018.go
@@ -30,18 +30,59 @@ func main() {
 		fmt.Println(err)
 	}
 
-	// adj := make([][]edge, len(ints))
-	// ints = ints[1:len(ints)]
+	adj := make([][]edge, len(ints))
 
-	// test := 0
-	// incr := 1
-	// for i := 0; i <= len(adj); i++ {
-	// 	if i > test {
-	// 		test += incr
-	// 		incr++
-	// 	}
-	// 	e1 := edge{to: i + 1, weight: i + 1}
-	// 	e2 := edge{to: i + 2, weight: i + 2}
-	// 	adj[i]
+	test := 0
+	incr := 1
+	for i := 0; i <= len(adj); {
+		if i >= test && i < test+incr {
+			// fmt.Println(i, i+incr)
+			// fmt.Println(i, i+incr+1)
+			left := edge{to: i + incr, weight: ints[i+incr]}
+			right := edge{to: i + incr + 1, weight: ints[i+incr+1]}
+			edges := []edge{left, right}
+			adj[i] = edges
+			i++
+		} else {
+			test += incr
+			incr++
+		}
+		if test+incr == len(adj) {
+			break
+		}
+	}
+	// spew.Dump(adj)
+
+	// Dijkstra (longest path instead of shortest path)
+	Q := map[int]bool{}
+	dist := make([]int, len(ints))
+	prev := make([]int, len(ints))
+	for i := 0; i < len(ints); i++ {
+		dist[i] = -1
+		prev[i] = -1 // Undefined
+		Q[i] = true
+	}
+	dist[0] = 0
+	// spew.Dump(Q)
+
+	// for len(Q) > 0 {
+	// 	u := maxI(dist)
+	// 	delete(Q, u)
+
+	// 	// for _, v := range adj[u] {
+	// 	// 	fmt.Println(v)
+	// 	// }
 	// }
+}
+
+func maxI(a []int) int {
+	maxV := -1
+	maxI := 0
+	for i, v := range a {
+		if v > maxV {
+			maxV = v
+			maxI = i
+		}
+	}
+	return maxI
 }
