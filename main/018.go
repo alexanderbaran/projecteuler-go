d284e803759a491088c878d983a6a9ffa88c38a1
Alexander Baran <alexanderbaran@live.com>
2016-11-10 01:35:58 +0100

    asdf

--- main/018.go
+++ main/018.go
@@ -30,17 +30,18 @@ func main() {
 		fmt.Println(err)
 	}
 
-	adj := make([][]edge, len(ints))
-	ints = ints[1:len(ints)]
+	// adj := make([][]edge, len(ints))
+	// ints = ints[1:len(ints)]
 
-	test := 0
-	incr := 1
-	for i := 0; i <= len(adj); i++ {
-		if i > test {
-			test += incr
-			incr++
-		}
-		e1 := edge{to: i + 1}
-		adj[i]
-	}
+	// test := 0
+	// incr := 1
+	// for i := 0; i <= len(adj); i++ {
+	// 	if i > test {
+	// 		test += incr
+	// 		incr++
+	// 	}
+	// 	e1 := edge{to: i + 1, weight: i + 1}
+	// 	e2 := edge{to: i + 2, weight: i + 2}
+	// 	adj[i]
+	// }
 }
