58ef68fe2781f600f050dff4760a59f10dad1037
Alexander Baran <alexanderbaran@live.com>
2016-11-14 05:24:54 +0100

    Completed 51.

--- main/051.go
+++ main/051.go
@@ -1,122 +1,95 @@
 package main
 
-import (
-	"bytes"
-	"fmt"
-	"strconv"
-)
-
 // import (
-// 	"bytes"
-// 	"strconv"
+// 	"fmt"
 // 	"time"
 // )
 
-func main() {
-	// defer timeTrack(time.Now())
+// var primeTable []bool
 
-	i := 123
-	// o := splitInt(i)
-	// s := splitInt(i)
+// func main() {
+// 	defer timeTrack(time.Now())
 
-	// for i := 50000; i <= 99999; i++ {
+// 	target := 8
+// 	n := int(1e6)
 
-	o := splitInt(i)
-	s := splitInt(i)
-	r := 8
+// 	primeTable = make([]bool, n)
+// 	for i := 3; i <= n; i += 2 {
+// 		if isPrime(i) {
+// 			primeTable[i] = true
+// 		}
+// 	}
 
-	for j := 0; j < len(o); j++ {
-		copy(s, o)
-		s[j] = r
-		fmt.Println(joinInts(s))
-		for i, _ := range s {
-			if i != j {
-				s[i] = r
-			} else {
-				s[i] = o[i]
-			}
-		}
-		fmt.Println(joinInts(s))
-	}
+// 	for i := 1; i < n; i++ {
+// 		o := splitInt(i)
+// 		s := make([]int, len(o))
 
-	for j := 0; j < len(o); j++ {
-		for k := j + 1; k < len(o); k++ {
-			copy(s, o)
-			s[j] = r
-			s[k] = r
-			fmt.Println(joinInts(s))
-			for i, _ := range s {
-				if i != j {
-					s[i] = r
-				} else {
-					s[i] = o[i]
-				}
-			}
-			fmt.Println(joinInts(s))
-		}
-	}
-}
+// 		for j := 0; j < len(o); j++ {
+// 			if replaceAndCheck(target, o, s, j) {
+// 				return
+// 			}
+// 		}
 
-// for i := 50000; i <= 99999; i++ {
-// 	for r := 0; r <= 9; r++ {
+// 		for j := 0; j < len(o); j++ {
+// 			for k := j + 1; k < len(o); k++ {
+// 				if replaceAndCheck(target, o, s, j, k) {
+// 					return
+// 				}
+// 			}
+// 		}
+
+// 		for j := 0; j < len(o); j++ {
+// 			for k := j + 1; k < len(o); k++ {
+// 				for l := k + 1; l < len(o); l++ {
+// 					if replaceAndCheck(target, o, s, j, k, l) {
+// 						return
+// 					}
+// 				}
+// 			}
+// 		}
 // 	}
 // }
+
+// func replaceAndCheck(target int, o, s []int, idx ...int) bool {
+// 	count := 0
+// 	fails := 0
+// 	primes := make([]int, 8)
+// 	for r := 0; r <= 9; r++ {
+// 		copy(s, o)
+// 		for _, i := range idx {
+// 			s[i] = r
+// 		}
+// 		d := joinInts(s)
+// 		if primeTable[d] && digitLength(d) == len(s) {
+// 			primes[count] = d
+// 			count++
+// 		} else {
+// 			fails++
+// 		}
+// 		if fails > 10-target {
+// 			return false
+// 		}
+// 		if count >= target {
+// 			fmt.Println(primes[0])
+// 			return true
+// 		}
+// 	}
+// 	return false
 // }
 
-func splitInt(n int) []int {
-	s := make([]int, digitLength(n))
-	for i := len(s) - 1; n > 0; i-- {
-		s[i] = n % 10
-		n = n / 10
-	}
-	return s
-}
+// func splitInt(n int) []int {
+// 	a := make([]int, digitLength(n))
+// 	for i := len(a) - 1; n > 0; i-- {
+// 		a[i] = n % 10
+// 		n = n / 10
+// 	}
+// 	return a
+// }
 
-func joinInts(s []int) int {
-	var buffer bytes.Buffer
-	for _, v := range s {
-		buffer.WriteString(strconv.Itoa(v))
-	}
-	i, _ := strconv.Atoi(buffer.String())
-	return i
-}
-
-// *x
-// x*
-
-// *xx
-// x*x
-// xx*
-
-// **x
-// *x*
-// x**
-
-// *xxx
-// x*xx
-// xx*x
-// xxx*
-
-// **xx
-// *x*x
-// *xx*
-// x**x
-// x*x*
-// xx**
-
-// *xxxx
-// x*xxx
-// xx*xx
-// xxx*x
-// xxxx*
-
-// **xxx
-// *x*xx
-// *xx*x
-// *xxx*
-// x**xx
-// x*x*x
-// x*xx*
-// xx**x
-// xx*x*
-// xxx**
+// func joinInts(a []int) int {
+// 	n := 0
+// 	for i := 0; i < len(a); i++ {
+// 		n = 10*n + a[i]
+// 	}
+// 	return n
+// }
